9dd6c4bba20ff8d8143493cf406526a968f35d1f
Rodrigo R. Almeidam <rodrigo.roberto.almeida@gmail.com>
2020-06-11 01:07:15 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './commom/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CardsComponent } from './home/components/cards/cards.component';
+import { SearchComponent } from './home/components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
